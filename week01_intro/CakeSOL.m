f9e742fa811deff4fe2f0ff4e7468a4ac0a385e7
Roy (MSI) <roywu2018@gmail.com>
2021-09-02 15:02:34 -0400

    commit demo

--- week01_intro/CakeSOL.m
+++ week01_intro/CakeSOL.m
@@ -9,11 +9,11 @@ clc, clear, close all
 CakeRadius= input('What is the cakes radius: ');
 CakeHeight= input('What is the cakes height: ');
 
-Area_top = 2*pi*(CakeRadius^2);%calculates area of the top and middle of the cake
-Cake_circum = pi*2*CakeRadius; %calculates the circumference of the cake
-Area_side = Cake_circum*CakeHeight; % calculates sides of the cake
+Area_top = 2*pi*(CakeRadius^2);      % calculates area of the top and middle of the cake
+Cake_circum = pi*2*CakeRadius;       % calculates the circumference of the cake
+Area_side = Cake_circum*CakeHeight;  % calculates sides of the cake
 
-Total_Cake_SA= Area_top + Area_side; %calculates total surface area
+Total_Cake_SA= Area_top + Area_side; % calculates total surface area
 
 %Prints out how much frosting we need in in^2
 fprintf('Amount of frosting needed for our cake (in^2): %f\n', Total_Cake_SA)
