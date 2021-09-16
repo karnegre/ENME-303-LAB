8ed07c5caab6335a289492452e39e36a94650958
roytwu <roywu2018@gmail.com>
2021-09-16 14:17:21 -0400

    adding switch in numbers for the tea problem

--- week02/Negrete_Karla_ENME303_Solutions.m
+++ week02/Negrete_Karla_ENME303_Solutions.m
@@ -16,8 +16,10 @@ clc, clear, close all
     else
         fprintf('%d is not a leap year \n', year)
     end
+%%* leap year: 2016
+%%* non-leap year: 2015
     
- %% Exercise 2: Tea Party
+ %% Exercise 2: Tea Party (if)
     fprintf('Tea options are white, green, oolong, black, and herbal\n')
     teatime= input('Would you like a cup of tea? Y or N\n', 's')
     
@@ -35,21 +37,40 @@ clc, clear, close all
        fprintf('Brew the tea for 3-6 min\n') 
    end
    
-%% Exercise 3: Tea Party Switch
-fprintf('Tea options are white, green, oolong, black, and herbal\n')
-    teatime= input('Would you like a cup of tea? Y or N\n', 's')
-    
+%% Exercise 3: Tea Party (switch)
+% fprintf('Tea options are white, green, oolong, black, and herbal\n')
+%     teatime= input('Would you like a cup of tea? Y or N\n', 's')
+%     
+%    if teatime == 'Y'
+%        tea = input('What kind of tea would you like?:\n ', 's');
+%    else 
+%        fprintf('Ok :( ')
+%    end
+%    
+% switch tea
+%     case {'white','green'} 
+%         fprintf('Brew the tea for 1-2 min\n')
+%     case {'black','oolong'}
+%         fprintf('Brew the tea for 2-3 min\n')
+%     otherwise
+%         fprintf('Brew the tea for 3-6 min\n')
+% end
+
+% using number as cases
+   fprintf('\n\n--- Tea problem is siwtch (number) ---\n')
+   fprintf('Tea options:\n 1=white, 1=green, 3=oolong, 3=black, and 4=herbal\n')
    if teatime == 'Y'
-       tea = input('What kind of tea would you like?:\n ', 's');
+       tea = input('What kind of tea would you like?:\n', 's');
    else 
        fprintf('Ok :( ')
    end
-   
+
 switch tea
-    case {'white','green'} 
+    case 1 
         fprintf('Brew the tea for 1-2 min\n')
-    case {'black','oolong'}
+    case 2
         fprintf('Brew the tea for 2-3 min\n')
     otherwise
         fprintf('Brew the tea for 3-6 min\n')
 end
+
