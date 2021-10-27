46b03e4f8c322d150085f1cc971dc643e1601925
roytwu <roywu2018@gmail.com>
2021-10-27 13:20:59 -0400

    adding one example for the user-defined function

---
+++ week07/userDefined.m
@@ -0,0 +1,16 @@
+a = [1 2 0; -2 -3 -4];
+aT= matxTranspose(a)
+
+
+function out =matxTranpose(x)
+    [num_row, num_col]=size(x);
+    newX=zeros(num_col,num_row);
+
+    for i=1:num_col
+        for j=1:num_row
+            newX(i,j)=x(j,i);
+        end
+    end
+    out =newX;
+end
+
