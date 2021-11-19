6527c5a8cb20d7c4e4cfb6cc2b094756fd16a844
Karla <86244818+karnegre@users.noreply.github.com>
2021-11-19 15:00:09 -0500

    Update Lab2_Assgn_Sol.m

--- week02/Lab2_Assgn_Sol.m
+++ week02/Lab2_Assgn_Sol.m
@@ -2,7 +2,7 @@
 %UMBC ID: MQ31578
 %Course: ENME 303
 %Description: Lab Assignment 2 Solutions...
-%Refer to Week 2_ Assignment.PDF for Exercise details
+%Refer to Week 2_ Assignment.pdf for Exercise details
 %Assignment location: Lab 2 folder in Google Drive
 %Updated: 2021.11.19 (KN)
 clc, clear, close all 
