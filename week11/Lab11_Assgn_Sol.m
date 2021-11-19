196e74b5301a8db8a95eba51b6bb882abb63b400
Karla <86244818+karnegre@users.noreply.github.com>
2021-11-19 16:36:09 -0500

    Update Lab11_Assgn_Sol.m

--- week11/Lab11_Assgn_Sol.m
+++ week11/Lab11_Assgn_Sol.m
@@ -1,46 +1,51 @@
 %Author: Karla Negrete
 %UMBC ID: MQ31578
 %Course: ENME 303
-%Description: Lab Assignment 11 Solutions
-clc, clear, close all
+%Description: Lab Assignment 11 Solutions...
+%Refer to Week 11_ Assignment.pdf for Exercise details
+%Assignment location: Lab 11 folder in Google Drive
+%Updated: 2021.11.19 (KN)
+clc, clear, close all 
 
-%% Prepare to work on the image
-%* read the image
-img = imread('e46.png');
-% img = imread('retriever.png');
+%% Exercise 1: Shearing
+    % Prepare to work on the image
+    %* read the image
+    img = imread('e46.png');
+    % img = imread('retriever.png');
 
-% get the dimensions of the image
-[x, y, z] = size(img);
-fprintf('dimension: %i, %i, %i\n\n', x, y, z);
+    % get the dimensions of the image
+    [x, y, z] = size(img);
+    fprintf('dimension: %i, %i, %i\n\n', x, y, z);
 
-R=zeros(x,y, 'uint8');
-G=zeros(x,y, 'uint8');
-B=zeros(x,y, 'uint8');
+    R=zeros(x,y, 'uint8');
+    G=zeros(x,y, 'uint8');
+    B=zeros(x,y, 'uint8');
 
-%% Shear transformation
-%* shear transformation matrix
-x_factor = 0;
-y_factor = 1;
-shear = [1 x_factor; y_factor 1]; 
+    %% Shear transformation
+    %* shear transformation matrix
+    x_factor = 0;
+    y_factor = 1;
+    shear = [1 x_factor; y_factor 1]; 
 
-for c=1:y     %* column index
-    for r=1:x %* row index
-        pxVal_1 =img(r, c, 1); 
-        pxVal_2 =img(r, c, 2);
-        pxVal_3 =img(r, c, 3);
+    for c=1:y     %* column index
+        for r=1:x %* row index
+            pxVal_1 =img(r, c, 1); 
+            pxVal_2 =img(r, c, 2);
+            pxVal_3 =img(r, c, 3);
 
-        before =[c;r]
-        after = shear * before
-        col = after(1)
-        row = after(2)
-        break
-%         display(row);
-        R(row, col) = pxVal_1; %* 0 to 255
-        G(row, col) = pxVal_2; %* 0 to 255
-        B(row, col) = pxVal_3; %* 0 to 255
+            before =[c;r]
+            after = shear * before
+            col = after(1)
+            row = after(2)
+
+            R(row, col) = pxVal_1; %* 0 to 255
+            G(row, col) = pxVal_2; %* 0 to 255
+            B(row, col) = pxVal_3; %* 0 to 255
+        end
     end
-end
-A = cat(3, R, G, B); %*concatenate 3 channels together
+    A = cat(3, R, G, B); %*concatenate 3 channels together
 
-imwrite(A, 'myImg.png')
+    imwrite(A, 'myImg.png')
+    
+%% Exercise 2: Reflecting Vectors
 
