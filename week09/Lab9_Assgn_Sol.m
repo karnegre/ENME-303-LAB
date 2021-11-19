74e11c79e400ea40bd45019b5d4071b01ae3de5b
Karla <86244818+karnegre@users.noreply.github.com>
2021-11-19 16:15:56 -0500

    Update Lab9_Assgn_Sol.m

--- week09/Lab9_Assgn_Sol.m
+++ week09/Lab9_Assgn_Sol.m
@@ -1,7 +1,10 @@
 %Author: Karla Negrete
 %UMBC ID: MQ31578
 %Course: ENME 303
-%Description: Lab Assignment 9 Solutions
+%Description: Lab Assignment 9 Solutions...
+%Refer to Week 9_ Assignment.pdf for Exercise details
+%Assignment location: Lab 9 folder in Google Drive
+%Updated: 2021.11.19 (KN)
 clc, clear, close all 
 
 %% Exercise 1: Matrix Transpose
